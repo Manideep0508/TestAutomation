c7a09350c4c11c68befefba10a2ad4ef464512fd
Shashidhar7407 <shashank.papishetty@gmail.com>
2022-11-25 15:43:43 +0530

    final change made by shashi

--- src/test/java/com/qa/testscripts/Testautimation.java
+++ src/test/java/com/qa/testscripts/Testautimation.java
@@ -12,17 +12,9 @@ public class Testautimation {
 	public static void main(String args[]) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver cdriver=new ChromeDriver();
-		cdriver.get("https://shopsystore.com/");
-		cdriver.findElement(By.linkText("Search")).click();
-		cdriver.findElement(By.name("q")).sendKeys("mens");
-		cdriver.findElement(By.className("action_button")).click();
-		cdriver.get("https://shopsystore.com/");
-		cdriver.findElement(By.linkText("Contact Us")).click();
-		cdriver.findElement(By.id("contactFormName")).sendKeys("shetty");
-		cdriver.findElement(By.id("contactFormEmail")).sendKeys("ppshetty@gmail.com");
-		cdriver.findElement(By.id("contactFormMessage")).sendKeys("Hope you are safe and happy i didn't recieve my reward");
-		cdriver.findElement(By.className("action_button")).click();
-	
+		System.out.println("hello manideep");
+		System.out.println("mounika hello");
+		
 		
 	}
 
